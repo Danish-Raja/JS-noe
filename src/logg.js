94e7582c286f4714bfbf155c58464a7f93bb0eac
Danish-Raja <daraa025@osloskolen.no>
2026-03-12 16:50:19 +0100

    tokerud
    lagde 2 page

---
+++ src/logg.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const knapp = document.getElementById("keapp");
+    knapp.addEventListener("click", function() {
+        window.location.href = "elev.html"; 
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const knapp = document.getElementById("kaapp");
+    knapp.addEventListener("click", function() {
+        window.location.href = "ansatte.html"; 
+    });
+});
---
+++ src/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const knapp = document.getElementById("knapp");
+    knapp.addEventListener("click", function() {
+        window.location.href = "logg.html"; 
+    });
+});
