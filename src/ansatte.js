6decd0c6e75ee0ce2db70e49ceb028f5f08f8c03
Danish-Raja <daraa025@osloskolen.no>
2026-03-14 16:18:03 +0100

    Noen ting jeg puttet på
    jeg lagde at når du klikker loggin så kommer du til ny website

---
+++ src/ansatte.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const knapp = document.getElementById("kfapp");
+    knapp.addEventListener("click", function() {
+        window.location.href = "menu.html"; 
+    });
+});
